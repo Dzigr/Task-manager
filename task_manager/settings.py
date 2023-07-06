0493c1996dfea8f03f757a6c45fcbda30b469f8d
Dzigr <dmbrag@gmail.com>
2023-07-06 18:08:10 +0300

    Add render to allowed hosts

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -39,6 +39,12 @@ ALLOWED_HOSTS = [
     'localhost',
 ]
 
+# Add render hosts to allowed for deploy
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
