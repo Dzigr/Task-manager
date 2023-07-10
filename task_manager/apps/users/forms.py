8d94e1d7914613340c5f8521b73fd9f4bc3dccdd
Dzigr <dmbrag@gmail.com>
2023-07-10 22:25:23 +0300

    Add users app

--- task_manager/apps/users/forms.py
+++ task_manager/apps/users/forms.py
@@ -2,14 +2,21 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
 
-class UserCreationForm(UserCreationForm):
+class NewUserCreationForm(UserCreationForm):
     """Class to register new user with required fields."""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['password1'].help_text = 'Ваш пароль должен содержать как минимум 3 символа.'
+        password_text = 'Ваш пароль должен содержать как минимум 3 символа.'
+        self.fields['password1'].help_text = password_text
 
     class Meta:
         model = get_user_model()
         fields = ('first_name', 'last_name', 'username',
                   'password1', 'password2')
+
+
+class UpdateForm(NewUserCreationForm):
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        return username
--- task_manager/apps/users/views.py
+++ task_manager/apps/users/views.py
@@ -5,7 +5,9 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 
-from .forms import UserCreationForm
+from .forms import NewUserCreationForm, UpdateForm
+from task_manager.mixins import UserAuthenticateMixin, CheckUserPermissionMixin
+from .models import User
 
 
 class UserListView(SuccessMessageMixin, ListView):
@@ -17,33 +19,42 @@ class UserListView(SuccessMessageMixin, ListView):
 
 class UserCreateView(SuccessMessageMixin, CreateView):
     """View for form to create a new user."""
-    model = get_user_model()
-    form_class = UserCreationForm
+    model = User
+    form_class = NewUserCreationForm
     template_name = 'users/create.html'
     success_url = reverse_lazy('login')
     success_message = _("Registration successful")
 
+    def form_valid(self, form):
+        return super().form_valid(form)
 
-class UserUpdateView(SuccessMessageMixin, UpdateView):
+    def form_invalid(self, form):
+        messages.error(request=self.request,
+                       message=_("Unsuccessful registration. Invalid information."))
+        return super().form_invalid(form)
+
+
+class UserUpdateView(UserAuthenticateMixin, CheckUserPermissionMixin,
+                     SuccessMessageMixin, UpdateView):
     """View to update existing user information."""
     model = get_user_model()
-    form_class = UserCreationForm
+    form_class = UpdateForm
     template_name = 'users/update.html'
     success_url = reverse_lazy('users')
     success_message = _('User information successfully updated')
-
-    def form_valid(self, form):
-        messages.success(request=self.request,
-                         message=self.success_message)
-        return super(UserCreationForm, self).form_valid(form)
+    permission_denied_message = _('You have no rights to change another user.')
+    permission_forwarded_url = 'users'
 
 
-class UserDeleteView(SuccessMessageMixin, DeleteView):
+class UserDeleteView(UserAuthenticateMixin, CheckUserPermissionMixin,
+                     SuccessMessageMixin, DeleteView):
     """View to delete existing user."""
     model = get_user_model()
     template_name = 'users/delete.html'
     success_message = _('User is successfully deleted')
     success_url = reverse_lazy('users')
+    permission_denied_message = _('You have no rights to change another user.')
+    permission_forwarded_url = 'users'
 
     def post(self, request, *args, **kwargs):
         return super().post(request, *args, **kwargs)
